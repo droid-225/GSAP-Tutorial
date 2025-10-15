2a5c1eca1223bc77559acb21f80876d7e768050a
Rishit Shah <aryanrishit1@gmail.com>
2025-10-15 23:49:10 +0530

    Commit 9[58:38]: Tried to make the navbar, some errors need to be fixed.

---
+++ jsm_gsap_landing/src/App.jsx
@@ -0,0 +1,16 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from 'gsap';
+import React from 'react';
+
+import Navbar from './components/Navbar.jsx';
+
+gsap.registerPlugin(ScrollTrigger, SplitText); // makes these available globally in project
+
+const App = () => {
+    return (
+        <main>
+            <Navbar/>
+        </main>
+    )
+}
+export default App
