1107e9b710ae66030227089bc82f98f01ee461ea
Rishit Shah <aryanrishit1@gmail.com>
2025-10-19 18:34:10 +0530

    Commit 11[1:12:08]: Made the first part of the Hero section.

--- jsm_gsap_landing/src/App.jsx
+++ jsm_gsap_landing/src/App.jsx
@@ -2,6 +2,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from 'gsap';
 
 import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText); // makes these available globally in project
 
@@ -9,6 +10,8 @@ const App = () => {
     return (
         <main>
             <Navbar/>
+            <Hero/>
+            <div className="h-dvh bg-black"/>
         </main>
     )
 }
